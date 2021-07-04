b5ea0c48a28791134766bab36abd4584a29d10f6
chinweokwu <paulcmorah@gmail.com>
2021-07-04 11:31:23 +0100

    remove calory directory

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,7 @@
-import CaloriesSaga from './Calories/sagas';
 import WorkoutsSaga from './workout/sagas'
 
 export default function* rootSaga () {
   yield[
-    CaloriesSaga,
     WorkoutsSaga
   ]
 };
